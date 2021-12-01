702402226bfaa9cd1de73f847f228b50ffd58426
Chrisjobling <chris.jobling@salecycle.com>
2021-12-01 11:18:30 +0000

    Added Longest common prefix

---
+++ src/main/kotlin/longestCommonPrefix/LongestCommonPrefix.kt
@@ -0,0 +1,23 @@
+package longestCommonPrefix
+
+/**
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+ */
+
+
+fun longestCommonPrefix(strs: Array<String>): String {
+    if (strs.isEmpty()) {
+        return ""
+    }
+    var prefix = strs[0]
+    for (i in 1 until strs.size){
+        print(strs[i])
+        while (strs[i].indexOf(prefix) !=0){
+            prefix = prefix.substring(0, prefix.length -1)
+            if (prefix.isEmpty()) return ""
+        }
+    }
+    return prefix
+}

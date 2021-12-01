d178e6388a31e7f3c490dd39464ba4c18e797cf6
Chrisjobling <chris.jobling@salecycle.com>
2021-12-01 14:03:57 +0000

    Added valid parentheses task

---
+++ src/main/kotlin/validParentheses/ValidParentheses.kt
@@ -0,0 +1,35 @@
+package validParentheses
+
+import java.util.*
+
+/**
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ **/
+
+fun isValid(s: String): Boolean {
+
+    var stack = ArrayDeque<Char>()
+    stack.sorted()
+    for (i in 0 until s.length){
+        var x = s[i]
+        if(x == '(' || x =='{' || x== '[' ){
+            stack.push(x)
+        }
+        if(stack.isEmpty()){
+            return false
+        }
+        when (x) {
+            ')' -> if (!stack.isEmpty() && stack.peek() == '(' ) {stack.pop()}else{return false}
+            '}' -> if (!stack.isEmpty() && stack.peek() == '{' ) {stack.pop()}else{return false}
+            ']' -> if (!stack.isEmpty() && stack.peek() == '[' ) {stack.pop()}else{return false}
+        }
+    }
+
+    return stack.isEmpty()
+
+}

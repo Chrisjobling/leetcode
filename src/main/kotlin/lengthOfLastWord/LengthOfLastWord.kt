908b8036776d3874ce11bf1fe98ddc2bd42f00f8
Chrisjobling <chris.jobling@salecycle.com>
2021-12-03 08:53:35 +0000

    Added length of last word with tests

---
+++ src/main/kotlin/lengthOfLastWord/LengthOfLastWord.kt
@@ -0,0 +1,15 @@
+package lengthOfLastWord
+
+fun lengthOfLastWord(s: String): Int {
+    if (s.isEmpty()) return 0
+    val trimmedString = s.trim()
+    var count = 0
+    for (i in trimmedString.length -1 downTo 0){
+        if(trimmedString[i] == ' '){
+            return count
+        }else {
+            count += 1
+        }
+    }
+    return count
+}

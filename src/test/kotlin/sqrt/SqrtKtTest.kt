e1cf802b3f11251225ff77c0e3f75caf8d556568
Chrisjobling <chris.jobling@salecycle.com>
2021-12-08 12:12:15 +0000

    Added sqrt, should improve this as quite slow

---
+++ src/test/kotlin/sqrt/SqrtKtTest.kt
@@ -0,0 +1,14 @@
+package sqrt
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SqrtKtTest {
+
+    @Test
+    fun mySqrt() {
+        assertEquals(2, mySqrt(4))
+        assertEquals(2, mySqrt(8))
+    }
+}

60f908aceba0729588462d090a9f5463c66d852b
Chrisjobling <chris.jobling@salecycle.com>
2021-12-09 19:52:18 +0000

    Added to lover case without using built in func

---
+++ src/test/kotlin/toLowerCase/ToLowerCaseKtTest.kt
@@ -0,0 +1,15 @@
+package toLowerCase
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ToLowerCaseKtTest {
+
+    @Test
+    fun toLowerCase() {
+        assertEquals("hello", toLowerCase("Hello"))
+        assertEquals("here", toLowerCase("here"))
+        assertEquals("lovely", toLowerCase("LOVELY"))
+    }
+}

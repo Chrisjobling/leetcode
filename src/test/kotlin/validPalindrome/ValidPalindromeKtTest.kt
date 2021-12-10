526427dc66952999637c666220cf2b3b7896ba9d
Chrisjobling <chris.jobling@salecycle.com>
2021-12-10 13:45:22 +0000

    Is valid palindrome for sentances

---
+++ src/test/kotlin/validPalindrome/ValidPalindromeKtTest.kt
@@ -0,0 +1,17 @@
+package validPalindrome
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ValidPalindromeKtTest {
+
+    @Test
+    fun isPalindrome() {
+//        assertTrue(isPalindrome("A man, a plan, a canal: Panama"))
+//        assertFalse(isPalindrome("race a car"))
+//        assertTrue(isPalindrome(" "))
+        assertFalse(isPalindrome("ab"))
+    }
+
+}
